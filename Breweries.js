5a6c297ea0f262bd115425645eb0a644aab586df
Matt <msm3390@gmail.com>
2021-06-10 06:23:40 -0400

    push

---
+++ Breweries.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import '../styles/SingleAttraction.css'
+
+export const Breweries = () => {
+    return (
+        <div>
+            <div className = 'home'>
+      <div className="jumbotron jumbotron-fluid abouthover">
+          <div className="breweriescontainer">
+              <h1>Breweries</h1>
+          </div>
+      </div>
+      <div className='aboutheader'>
+          <h1>The Queen City <span className='crown'><i class="fas fa-crown fa-xs"></i></span> Porkopolis <span className='crown'><i class="fas fa-crown fa-xs"></i></span> The City That Sings <span className='crown'><i class="fas fa-crown fa-xs"></i></span> The 'Nati</h1>
+          <h2>WHATEVER NAME YOU KNOW US BY, THERE'S ONLY ONE CINCINNATI</h2>
+      </div>
+        </div>
+            <div className = 'singleattraction'>
+                <a href="https://rhinegeist.com/" target="_blank"><h1>Rhinegeist Brewery</h1></a>
+            <img src="https://images2.imgbox.com/4a/d7/dNyZJw07_o.png" alt=" Rhinegeist image"/>
+            <p>1910 Elm St, Cincinnati, OH 45202</p>
+
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://www.listermannbrewing.com/" target="_blank"><h1>Listermann Brewing Company</h1></a>
+            <img src=" https://images2.imgbox.com/f3/12/DAOXumCR_o.png " alt="Listermann img"/>
+            <p>1621 Dana Ave, Cincinnati, OH 45207</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href=" https://www.madtreebrewing.com/ " target="_blank"><h1>MadTree Brewing</h1></a>
+            <img src= "https://images2.imgbox.com/9a/6b/OtZHfIex_o.png" alt="Madtree img"/>
+            <p>3301 Madison Rd, Cincinnati, OH 45209</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://www.woodburnbrewing.com/" target="_blank"><h1>Woodburn Brewing</h1></a>
+            <img src="https://images2.imgbox.com/5d/e9/tQwPX9d1_o.png" alt=""/>
+            <p>2800 Woodburn Ave, Cincinnati, OH 45206</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://www.humblemonkbrewing.com/"target="_blank"><h1>Humble Monk Brewing</h1></a>
+            <img src="https://images2.imgbox.com/61/32/1omces5f_o.png" alt=""/>
+            <p>1641 Blue Rock St, Cincinnati, OH 45223</p>
+            </div>
+
+            
+            <div className = 'singleattraction'>
+                <a href="https://www.westsidebrewing.com/" target="_blank"><h1>West Side Brewing</h1></a>
+            <img src="https://images2.imgbox.com/92/d8/91H8Jx6y_o.png" alt=""/>
+            <p>3044 Harrison Ave, Cincinnati, OH 45211</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://fiftywestbrew.com/" target="_blank"><h1>Fifty West</h1></a>
+            <img src="https://images2.imgbox.com/dc/bd/tjk7sMhp_o.png" alt=""/>
+            <p>7668 Wooster Pike, Cincinnati, OH 45227</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://www.13belowbrewery.com/"target="_blank"><h1>13 Below Brewery</h1></a>
+            <img src="https://images2.imgbox.com/36/b4/28bFiiFI_o.png" alt=""/>
+            <p>7391 Forbes Rd, Cincinnati, OH 45233</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://www.braxtonbrewing.com/" target="_blank"><h1>Braxton Brewing Company</h1></a>
+            <img src="https://images2.imgbox.com/2c/56/rR3QVArW_o.png"alt=""/>
+            <p>27 W 7th St, Covington, KY 41011</p>
+            </div>
+
+
+            <div className = 'singleattraction'>
+                <a href="https://pedalwagon.com/cincinnati/" target="_blank"><h1>Pedal Wagon</h1></a>
+            <img src="https://images2.imgbox.com/ce/83/dEa0Vqdg_o.png" alt=""/>
+            <p>1114 Bunker Alley, Cincinnati, OH 45202</p>
+            </div>
+        </div>
+    )
+}
+
+export default Breweries
